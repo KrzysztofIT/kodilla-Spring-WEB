bc1ae350b97825147cf338867c0dfcfe8fac59bb
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-08-28 21:32:24 +0200

    third attempt to 18.2

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TrelloClient {
@@ -30,6 +31,14 @@ public class TrelloClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    private URI getUriToTrello(){
+        return  UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken).build().encode().toUri();
+
+
+    }
+
     public List<TrelloBoardDto> getTrelloBoards(){
 
         //TrelloBoardDto[] boardsResponse = restTemplate.getForObject("htpps://api.trello.com/1/mmebers/krzysztofwasilewski10/boards"
@@ -37,17 +46,19 @@ public class TrelloClient {
         //TrelloBoardDto[] boardsResponse = restTemplate.getForObject(trelloEndpoint + "/members/krzysztofwasilewski10/boards"
         //        + "?key=" trelloAppKey + "?token=" trelloToken,
         //        TrelloBoardDto[].class);
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+        /*URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
                 .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken).build().encode().toUri();
+                .queryParam("token", trelloToken).build().encode().toUri();*/
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getUriToTrello(), TrelloBoardDto[].class);
 
+        /*
         if (boardsResponse != null){
             return Arrays.asList(boardsResponse);
         }
         return new ArrayList<>();
-
+        */
+        return Optional.ofNullable(Arrays.asList(boardsResponse)).orElse(new ArrayList<>());
 
     }
 
