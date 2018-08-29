89812cb4013aaf9f163073dd38642cd2ff7ea969
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-08-29 22:26:48 +0200

    second  attempt to 18.3

--- src/main/java/com/crud/tasks/domain/AttachmentByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentByType.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AttachmentByType {
     private Trello trello;
 }
--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Trello {
     private int board;
     private int card;
