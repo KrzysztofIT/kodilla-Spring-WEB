7fab7f451227114b5e6f19a4c309229ec588e8ec
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-09-14 19:04:12 +0200

    fifth attempt to 21.1

--- src/test/java/com/crud/tasks/trello/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/trello/mapper/TaskMapperTest.java
@@ -19,7 +19,7 @@ public class TaskMapperTest {
     private TaskMapper taskMapper;
 
     @Test
-    public void checkTestMapperDto ()  {
+    public void checkTestMapper ()  {
         //Given
 
         TaskDto taskDto = new TaskDto(1L,"test TaskDto title","test TaskDto content");
